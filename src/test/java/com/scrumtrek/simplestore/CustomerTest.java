58b47dec417b670ade097f2e86a8874df0ae2113
Alexander Komarov <protven@gmail.com>
2016-04-04 14:48:33 +0300

    add test class

---
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -0,0 +1,17 @@
+package com.scrumtrek.simplestore;
+
+import static org.junit.Assert.*;
+
+
+public class CustomerTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+}
