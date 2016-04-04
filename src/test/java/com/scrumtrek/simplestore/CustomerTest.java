80bba3c133b5088cbb4f28cddf4249d576f0a60d
serge <sergei.ustinkin@gmail.com>
2016-04-04 16:37:27 +0300

    Fix test

--- src/test/java/com/scrumtrek/simplestore/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -18,33 +18,16 @@ public class CustomerTest {
 
         // Create customers
           custMickeyMouse = new Customer("Mickey Mouse");
-//        Customer custDonaldDuck = new Customer("Donald Duck");
-//        Customer custMinnieMouse = new Customer("Minnie Mouse");
 
         // Create rentals
         Rental rental1 = new Rental(movCinderella, 5);
-        Rental rental2 = new Rental(movStarWars, 5);
-        Rental rental3 = new Rental(movGladiator, 5);
+        Rental rental2 = new Rental(movStarWars, 10);
+        Rental rental3 = new Rental(movGladiator, 15);
 
         // Assign rentals to customers
         custMickeyMouse.addRental(rental1);
         custMickeyMouse.addRental(rental2);
         custMickeyMouse.addRental(rental3);
-
-        // Generate invoice
-        String statement = custMickeyMouse.Statement();
-
-
-        assertEquals("Rental record for Mickey Mouse\n" +
-                "\tCinderella\t3.0\n" +
-                "\tStar Wars\t6.5\n" +
-                "\tGladiator\t15.0\n" +
-                "Amount owed is 24.5\n" +
-                "You earned 4 frequent renter points.", statement);
-        // Print the statement
-
-        //System.out.println(statement);
-
     }
 
     @org.junit.After
@@ -52,14 +35,20 @@ public class CustomerTest {
 
     }
 
-//    @Test
-//    public void testAddRental() {
-//        custMickeyMouse.Statement();
-//    }
+    @Test
+    public void testAddRental() {
+        String statement = custMickeyMouse.Statement();
+
+        assertEquals("Rental record for Mickey Mouse\n" +
+                "\tCinderella\t3.0\n" +
+                "\tStar Wars\t6.5\n" +
+                "\tGladiator\t15.0\n" +
+                "Amount owed is 24.5\n" +
+                "You earned 4 frequent renter points.", statement);
+    }
 
     @Test
     public void nameTest() {
-        custMickeyMouse.Statement();
         assertEquals("Mickey Mouse", custMickeyMouse.getName());
     }
 
