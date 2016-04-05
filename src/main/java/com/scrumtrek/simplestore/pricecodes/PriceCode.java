e6a45d193790688bc2863c6543c7584cedb7a802
serge <sergei.ustinkin@gmail.com>
2016-04-05 14:44:33 +0300

    Add docs

--- src/main/java/com/scrumtrek/simplestore/pricecodes/PriceCode.java
+++ src/main/java/com/scrumtrek/simplestore/pricecodes/PriceCode.java
@@ -7,13 +7,25 @@ package com.scrumtrek.simplestore.pricecodes;
 public abstract class PriceCode {
     private String name;
 
+    /**
+     * @return pricecode name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * set pricecode name
+     * @param name
+     */
     protected void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * count AMOUNT
+     * @param daysRented
+     * @return
+     */
     public abstract double getAmount(int daysRented);
 }
